5cd8835c6df0411e4d11f87dd411e210bb84529b
Udhayaboopathi <udhayaboopathi2003@gmail.com>
2025-12-18 15:01:48 +0530

    feat: enable CORS credentials in axios instance configuration

--- lib/api.ts
+++ lib/api.ts
@@ -5,6 +5,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true, // Enable CORS credentials
 });
 
 // Request interceptor to add auth token
